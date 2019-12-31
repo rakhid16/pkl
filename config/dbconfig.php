d68262f721c7cf866deeb725fc697ff2f3074199
Rakhman Wahid <roploverz@gmail.com>
2019-12-31 10:10:33 +0700

    punya kolel teredit

--- config/dbconfig.php
+++ config/dbconfig.php
@@ -1,28 +1,19 @@
 <?php  
+	function connDB(){
+		$dbServer = 'localhost';
+		$dbUser = 'root';
+		$dbPass = '';
+		$dbName = 'users';
 
-function connDB(){
-	$dbServer = 'localhost';
-	$dbUser = 'root';
-	$dbPass = '';
-	$dbName = 'magang_pertamina';
+		$conn = mysqli_connect($dbServer, $dbUser, $dbPass);
 
-	$conn = mysqli_connect($dbServer, $dbUser, $dbPass);
+		if (! $conn) {
+			die("Koneksi Gagal");
+		}
+		else{
+			mysqli_select_db($conn, $dbName);
+		}
 
-	if (! $conn) {
-		die("Koneksi Gagal");
+		return $conn;
 	}
-	else{
-		mysqli_select_db($conn, $dbName);
-		// echo "Koneksi Sukses";
-	}
-
-	return $conn;
-
-}
-
-
-
-
-
-
 ?>
--- login/index.php
+++ login/index.php
@@ -1,77 +1,76 @@
-<?php  
-include '../config/dbconfig.php';
+<?php
+  include '../config/dbconfig.php';
 ?>
 
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
+  <head>
+    <title>Laman Login</title>
 
-<head>  
-  <title>Laman Login</title>
+    <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1">
 
-  <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/pertamina/static/css/formLogin.css" />
 
-  <link rel="stylesheet" href="/pertamina/static/css/formLogin.css" />
+    <link rel="stylesheet" href="/pertamina/static/css/login.css" />
+    <link rel="shortcut icon" type="image/png" href="/pertamina/static/img/favicon.png"/>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+  </head>
 
-  <link rel="stylesheet" href="/pertamina/static/css/login.css" />
-  <link rel="shortcut icon" type="image/png" href="/pertamina/static/img/favicon.png"/>
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
-</head>
-
-<body>
-
-<div class="left-side">
-    <center><img src="work.svg">
-      <p><h1>Lorem Ipsum!</h1>Dolor si amet</p>
+  <body>
+    <div class="left-side">
+      <center>
+        <img src="../static/img/work.svg">
+        <h1 style="font-size: 200%; font-weight:bold">Selamat datang!</h1><br>
+        Pertamina Unit Pemasaran V Bertanggung Jawab terhadap<br>
+        Pemasaran Produk-produk Pertamina di Wilayah<br>
+        Jawa Timur, Kepulauan Nusa Tenggara, dan Sekitarnya<br><br>
+        <a href="https://www.pertamina.com/" target=blank><button >Website Utama</button></a>
+      </center>
     </div>
-<div class="right-side">
 
-<div style="background: white; height: 460px; width: 450px; margin-left: 120px; margin-top: 30px;border-radius: 25px">
-  <center>
-          <img src="../static/img/logo.png" width="40%" height="50%">
-  </center>  
+    <div class="right-side">
 
-<form class="form" action="/pertamina/routes/routeLogin.php" method="post">
-      
-      <?php 
-        if(isset($_GET['status'])){
-          if($_GET['status']=="gagal"){
-            echo "<p><font color=red> Nama dan Password tidak sesuai !</font></p>";
-          }
-        }
-      ?>
+      <!-- DIV FOR LOGIN BOX -->
+      <div style="background: white; margin: 120px 120px 120px 120px;border-radius: 25px">
+        <center>
+                <img src="../static/img/logo.png" width="40%" height="50%">
+        </center>  
 
-      <p class="field" style="margin-left: 100px; margin-top: 10px">
-        <input type="text" name="nama" placeholder="Nama" required/>
-        <i class="fa fa-user"></i>
-      </p>
+        <form class="form" action="/pertamina/routes/routeLogin.php" method="post">
+          <!-- ACCESS DATABASE FOR LOGIN -->
+          <?php 
+            if(isset($_GET['status'])){
+              if($_GET['status']=="gagal"){
+                echo '<center style="color:#DA251C; font-weight: bold">E-mail dan/atau Kata Sandi salah!</center>';
+              }
+            }
+          ?>
 
-      <p class="field" style="margin-left: 100px;">
-        <input type="password" name="sandi" placeholder="Sandi" required/>
-        <i class="fa fa-lock"></i>
-      </p>
+          <!-- USERNAME, PASSWORD, & SUBMIT -->
+          <p class="field" style="margin-left: 85px; margin-top: 10px">
+            <input type="text" name="nama" placeholder="E-mail" required/>
+            <i class="fa fa-user"></i>
+          </p>
+          <p class="field" style="margin-left: 85px;">
+            <input type="password" name="sandi" placeholder="Kata Sandi" required/>
+            <i class="fa fa-lock"></i>
+          </p>
+          <center>
+            <p class="submit"><input type="submit" name="commit" value="Masuk" style="background: #DA251C"></p>
+          </center>
 
-<center>
-      <p class="submit"><input type="submit" name="commit" value="Masuk" style="background: #DA251C"></p>
-</center>
+          <!-- AFTER SUBMIT BUTTON -->
+          <p class="remember" style="margin-left: 45px">
+            <input type="checkbox" id="remember" name="remember" />
+            <label for="remember"><span></span>Simpan Akun</label>
+            <a style="color: #DA251C; float: right; margin-right: 75px; font-size: 14px;" href="#">Lupa Sandi?</a>
+          </p>
+          <center style="margin-bottom:10px">
+            Belum punya akun? <a href="#" style="color: #DA251C; font-size: 14px"> Daftar!</a>
+          </center>
 
-      <p class="remember" style="margin-left: 45px">
-        <input type="checkbox" id="remember" name="remember" />
-        <label for="remember"><span></span>Remember Me</label>
-        <a style="color: red; float: right; margin-right: 75px; font-size: 14px; font-style: italic; font-weight: bold;" href="#">Lupa Sandi?</a>
-      
-      </p>
+        </form>        
+      </div>
+    </div>
 
- <center>
-<p>
-  Don't have an account? <a href="" style="color: red; font-weight: bold; font-size: 14px">Sign Up</a> 
-</p>
-</center>
-    </form>
-
-
-</div>
-
-</div>
-
-</body>
+  </body>
 </html>
