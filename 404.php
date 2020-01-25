4539a1b47e72e456aa56916bfac866d1f5239d7f
kholilboy <kholil.zboy@gmail.com>
2020-01-25 12:46:41 +0700

    refactoring AssetHome + add  404 URL

---
+++ 404.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <!--IE Compatibility modes-->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!--Mobile first-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Error - 404</title>
+    
+    <meta name="description" content="Free Admin Template Based On Twitter Bootstrap 3.x">
+    <meta name="author" content="">
+    
+    <meta name="msapplication-TileColor" content="#5bc0de" />
+    
+    <meta name="msapplication-TileImage" content="assets/img/metis-tile.png" />
+    
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="../pertamina/static/assetRoute/bootstrap.css">
+    
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="../pertamina/static/assetRoute/font-awesome.css">
+    
+    <!-- Metis core stylesheet -->
+    <link rel="stylesheet" href="../pertamina/static/assetRoute/main.css">
+    
+    <!-- metisMenu stylesheet -->
+    <link rel="stylesheet" href="../pertamina/static/assetRoute/metisMenu.css">
+    
+    <!-- onoffcanvas stylesheet -->
+    <link rel="stylesheet" href="../pertamina/static/assetRoute/onoffcanvas/onoffcanvas.css">
+    
+    <!-- animate.css stylesheet -->
+    <link rel="stylesheet" href="../pertamina/static/assetRoute/animate.css/animate.css">
+    <!-- Ikon Fav -->
+    <link rel="shortcut icon" type="image/x-icon" href="../pertamina/static/img/favicon.png">
+</head>
+
+<body class="error" style="background-color: #d1cbcb !important">
+    <div class="container" style="margin-top: 5%">
+        <div class="col-lg-8 col-lg-offset-2 text-center">
+            <div class="logo">
+                <h1>404</h1>
+            </div>
+            <img src="../pertamina/static/assetRoute/gif/kocheng.gif">
+            <p style="font-weight: bold;" class="lead text-muted">URL Not Found, Please Back to the World.</p>
+            <div class="clearfix"></div>
+            
+            <div class="clearfix"></div>
+            <div class="sr-only">
+                &nbsp;
+            </div>
+            <br>
+            <div class="col-lg-6 col-lg-offset-3">
+                <div class="btn-group btn-group-justified">
+                    <a href="/pertamina/home" class="btn btn-info">Return Dashboard</a>
+                    <a href="https://www.pertamina.com" class="btn btn-warning">Return Website</a>
+                </div>
+            </div>
+        </div>
+        <!-- /.col-lg-8 col-offset-2 -->
+    </div>
+</body>
+
+</html>
+
+
